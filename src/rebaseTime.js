e179b594fe56ff45ffff08e1ee83248f82c8b78d
sahel95 <sahel.baghdadi@gmail.com.com>
2021-11-18 18:43:38 +0330

    rebase time

--- src/rebaseTime.js
+++ src/rebaseTime.js
@@ -1,7 +1,25 @@
 const Web3 = require('web3')
 const subscribeToContract = require('./subscribeToContract')
+const connectToProvider = require('./connector')
 
 const EPOCH_INTERVAL = 28800;
+
+
+const prettifySeconds = (seconds) => {
+  if (seconds !== 0 && !seconds) {
+    return "";
+  }
+
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+
+  const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+  const hDisplay = h > 0 ? h + (h == 1 ? " Hour, " : " Hours, ") : "";
+  const mDisplay = m > 0 ? m + (m == 1 ? " Min" : " Mins") : "";
+  console.log(dDisplay + hDisplay + mDisplay);
+  return dDisplay + hDisplay + mDisplay;
+}
 
 
 const secondsUntilBlock = (startBlock, endBlock) => {
@@ -12,22 +30,25 @@ const secondsUntilBlock = (startBlock, endBlock) => {
   };
 
 
-const timeUntilRebase = async (provider) => {
+const timeUntilRebase = async () => {
+    const provider = connectToProvider()
     const web3 = new Web3(provider)
 
     const currentBlock = await web3.eth.getBlockNumber()
     const currentBlockTime = (await web3.eth.getBlock(currentBlock)).timestamp
-
-    const stakingContract = subscribeToContract('STAKING_ADDRESS', provider)
+    const stakingContract = subscribeToContract('TimeStaking', provider)
     const epoch = await stakingContract.methods.epoch().call()
     const nextRebase = epoch.endTime;
 
     if (currentBlockTime && nextRebase) {
         const seconds = secondsUntilBlock(currentBlockTime, nextRebase);
         // return prettifySeconds(seconds);
+        console.log(seconds);
         return seconds
       }
 }
+
+timeUntilRebase()
 
 
 module.exports = timeUntilRebase
