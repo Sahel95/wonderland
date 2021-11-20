718e1630cf6e479b930536c85a49c222522d2354
sahel95 <sahel.baghdadi@gmail.com.com>
2021-11-20 14:21:26 +0330

    add infux writer

---
+++ src/influx/client.js
@@ -0,0 +1,9 @@
+const clientData = {
+    token: 'S44XHxVDSm0iaalMko2I8hKD0JAPw0Zea776qp9LZ4HIqNcNaugn0zmYZEg-yvYDQo5qaW8Ys2UEqWWT5-gXFw==',
+    org: 'manwe',
+    bucket: 'manbuck',
+    url: 'https://influx.ssdnodes.manko.yoga'
+    // url: 'http://172.17.0.6:8086'
+}
+
+module.exports = clientData
---
+++ src/influx/influxWriter.js
@@ -0,0 +1,36 @@
+const {InfluxDB, Point, HttpError} = require('@influxdata/influxdb-client')
+const {url, token, org, bucket} = require('./client')
+const {hostname} = require('os')
+
+const influxWriter = async (discount, bond) => {
+    const tableName = `${bond}`
+
+    const client = new InfluxDB({
+        url: url,
+        token: token
+    })  
+    
+    const writeApi = client.getWriteApi(org, bucket)
+    writeApi.useDefaultTags({ host: 'host1' })
+    const point = new Point(tableName)
+    // .tag('price', price)
+    .floatField('discount', discount)
+    .timestamp(new Date())
+    writeApi.writePoint(point)
+    
+    await writeApi
+        .close()
+        .then( () => {
+            console.log('Influx FINISHED');
+        })
+        .catch( e => {
+            console.error(e);
+            console.log('Influx Finished ERROR');
+        })
+    return
+}
+
+// influxWriter(122, 220, 'ss', 'rtyui')
+
+module.exports = influxWriter
+
